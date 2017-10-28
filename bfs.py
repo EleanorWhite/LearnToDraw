a76f1b9a628f7641f53f089737c74cf2e26454e1
Nelll White <nwhite@cs.hmc.edu>
2017-10-28 00:50:24 -0700

    change to backtracking pixel thing

--- bfs.py
+++ bfs.py
@@ -9,6 +9,7 @@ def cornerFind(arr):
     while not len(fringe) == 0:
         current = fringe.pop()
         print("current:", current)
+        print("fringe", fringe)
         currentNode = current[0]
         # if we haven't seen this pixel yet
         if currentNode not in closed:
@@ -17,13 +18,26 @@ def cornerFind(arr):
                 closed.append(currentNode)
                 corners.append(currentNode)
                 nodeNeighbors = neighbors(currentNode)
-                print("Node neighbors", nodeNeighbors)
+                #print("Node neighbors", nodeNeighbors)
+
+                leftNeighbor = None
+                rightNeighbor = None
                 for neighbor in nodeNeighbors:
                     #print("neighbor", neighbor)
 
                     if neighbor[0] not in closed and neighbor[0] in arr:
-                        fringe.append(neighbor)
-            # line search 
+                        if leftNeighbor == None:
+                            leftNeighbor = neighbor[0]
+                            rightNeighbor = neighbor[0]
+                        else:
+                            rightNeighbor = neighbor[0]
+                    elif leftNeighbor != None and neighbor[0] not in closed:
+                        centerNeighbor = ((leftNeighbor[0]+rightNeighbor[0])/2, (leftNeighbor[1]+rightNeighbor[1])/2)
+                        direction = (centerNeighbor[0]-currentNode[0], centerNeighbor[1]-currentNode[1])
+                        fringe.append((centerNeighbor, direction))
+                        leftNeighbor = None
+                        rightNeighbor = None
+            # line sarc    h 
             else:
                 direct = current[1]
                 nextNode = (currentNode[0]+direct[0], currentNode[1]+direct[1])
@@ -38,22 +52,29 @@ def cornerFind(arr):
     return corners
 
 def neighbors(node):
-    radius = 15
+    """ This takes in a node, and returns a sampling of the nodes
+    15 pixels away from it"""
+    
+    radius = 5
     neighbors = []
 
-    neighbors.append((radius,radius))
-    neighbors.append((radius,0))
-    neighbors.append((radius,-radius))
-    neighbors.append((0, radius,))
-    neighbors.append((0, -radius))
-    neighbors.append((-radius, radius))
-    neighbors.append((-radius,0))
-    neighbors.append((-radius,-radius))
-    #for i in range(-radius, radius+1):
-    #   neighbors.append((radius-1,i))
-    #   neighbors.append((-1*radius+1, i))
-    #   neighbors.append((i, radius-1))
-    #   neighbors.append((i, -1*radius+1))
+    #neighbors.append((radius,radius))
+    #neighbors.append((radius,0))
+    #neighbors.append((radius,-radius))
+    #neighbors.append((0, radius,))
+    #neighbors.append((0, -radius))
+    #neighbors.append((-radius, radius))
+    #neighbors.append((-radius,0))
+    #neighbors.append((-radius,-radius))
+    for i in range(-radius, radius+1):
+        neighbors.append((-radius, i))
+    for i in range(-radius, radius+1):
+        neighbors.append((i, radius))
+    for i in range(radius-1, -radius, -1):
+        neighbors.append((radius, i))
+    for i in range(radius-1, -radius,-1):
+        neighbors.append((i, -radius))
+
     for i in range(len(neighbors)):
         neighbors[i] = ((neighbors[i][0]+node[0], neighbors[i][1]+node[1]),neighbors[i])
 
@@ -76,11 +97,69 @@ def neighbors(node):
     return neighbors
 '''
 
+
+def conSort(img):
+    tooFar = 1000
+    tolerance = 10
+    path = []
+    path.append(img[0])
+    img = img[1:]
+    backtrack = -2
+    while img != []:
+        closestDist = float("inf")
+        bestPix = None
+        currPixel = path[-1]
+        for pix in img:
+            dist = (currPixel[0]-pix[0])**2 + (currPixel[1]-pix[1])**2
+            if dist < closestDist + tolerance:
+                bestPix = pix
+                closestDist = dist
+            elif (dist - closestDist) < tolerance and len(path) > 1:
+                prevPix = path[-2]
+                direct = (currPixel[0]-prevPix[0],currPixel[1]-prevPix[1])
+                bestPixDirect = (bestPix[0]-currPixel[0], bestPix[1]-currPixel[1])
+                otherDirect = (pix[0]-currPixel[0], pix[1]-currPixel[1])
+                
+                bestPixDist = (direct[0]-bestPixDirect[0])**2 + (direct[1]-bestPixDirect[1])**2
+                otherDist = (direct[0]-otherDirect[0])**2 + (direct[1]-otherDirect[1])**2
+
+                if bestPixDist > otherDist:
+                    bestPix = pix
+
+        if closestDist > tooFar:
+            #return path
+            # backtrack
+            if len(path) < -backtrack: #check for off by one error
+                return path
+            path.append(path[backtrack])
+            backtrack -= 2
+        else:
+            backtrack = -2
+            path.append(bestPix)
+            img.remove(bestPix)
+        
+    return path
+
+
 def useImage(filename):
     img = edgeDetection.getImageArray(filename)
-    return cornerFind(img)
+    print(img)
+    
+    newImg = conSort(img)
+    maxDist = 0
+    biggestJump = 0
+    prev = newImg[0]
+    for i in newImg[1:]:
+        dist = (prev[0]-i[0])**2 + (prev[1]-i[1])**2
+        prev = i
+        if dist > maxDist:
+            maxDist = dist
+            biggestJump = i
+    print(maxDist, biggestJump)
+
+    return newImg
 
 def main():
-    print(useImage('rectangle.png'))
+    print(useImage('star.png'))
 
 if __name__ == "__main__": main()
--- edgeDetection.py
+++ edgeDetection.py
@@ -15,8 +15,8 @@ def getImageArray(filename):
 
     # get coordinates for zero pixels
     coords = []
-    for i in range(len(redImg)):
-        for j in range(len(redImg[i])):
+    for i in range(0,len(redImg),3):
+        for j in range(0,len(redImg[i]),3):
             if redImg[i][j] == 0:
                 coords.append((i,j))
 
